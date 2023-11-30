b08095108f5cd3dcaa353f050eb2a7218a0468b4
Nikhilrenjith <42229967+Nikhilrenjith@users.noreply.github.com>
2023-11-30 20:24:14 +0530

    Added Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import Home from "./Components/Home";
+import Sidebar from "./Components/Sidebar";
+import Dashboard from "./Components/Dashboard";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Home />
+    <div className="">
+      <div className="flex overflow-scroll ">
+        <div className="basis-[12%] h-[100vh]">
+          <Sidebar />
+        </div>
+        <div className="basis-[88%] border overflow-scroll h-[100vh]">
+          <Dashboard />
+          <div>
+            <Outlet></Outlet>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ client/src/Components/Main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = () => {
+  return <div>Working</div>;
+};
+
+export default Main;
